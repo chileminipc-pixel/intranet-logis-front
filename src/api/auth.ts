023b40846dfadfba100833f912da9b226897add6
Alvaro <chileminipc@gmail.com>
2025-09-26 02:30:00 -0300

    Fix auth, estaba en dev aun

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -17,10 +17,13 @@ export const login = async (
   email: string,
   password: string
 ): Promise<LoginResponse> => {
-const response = await axios.post<LoginResponse>(
-  "http://localhost:3000/login",
-  { email, password }
-);
+  const response = await axios.post<LoginResponse>(
+    "https://intranet-logis-backend-production.up.railway.app/login",
+    { email, password }
+  );
+  return response.data;
+};
+
 console.log("Respuesta del servidor:", response); // Verifica la respuesta completa
 return response.data;
 
